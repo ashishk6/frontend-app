05999191d29a60e5dc3b81b1b5620977a0fcc114
saikumar <saikumarnanepally@gmail.com>
2020-10-27 13:10:11 +0100

    configured the end point

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,9 @@
-export const BLOG_URL = '/posts';
-export const BLOG_FETCH_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/blog/getBlogDetails';
-export const BLOG_ADD_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/blog/saveBlog';
-export const BLOG_EDIT_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/blog/editBlog';
-export const BLOG_DELETE_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/blog/deleteBlog';
-export const BLOG_LOGIN_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/user/login';
-export const BLOG_REGISTER_URL = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/user/registerUser';
+const BACKEND_ENDPOINT_TEMP = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/';
+const BACKEND_ENDPOINT = process.env.BACKEND_ENDPOINT || BACKEND_ENDPOINT_TEMP;
+
+export const BLOG_FETCH_URL = BACKEND_ENDPOINT + 'blog/getBlogDetails';
+export const BLOG_ADD_URL =  BACKEND_ENDPOINT + 'blog/saveBlog';
+export const BLOG_EDIT_URL =  BACKEND_ENDPOINT + 'blog/editBlog';
+export const BLOG_DELETE_URL = BACKEND_ENDPOINT + 'blog/deleteBlog';
+export const BLOG_LOGIN_URL = BACKEND_ENDPOINT + 'user/login';
+export const BLOG_REGISTER_URL = BACKEND_ENDPOINT + 'user/registerUser';
