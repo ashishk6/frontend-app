587c0d17e8f4db6674101137a3f0b24531819022
ashishk6 <37957126+ashishk6@users.noreply.github.com>
2020-11-01 16:13:28 +0100

    Update urls.js
    updating the var

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 const BACKEND_ENDPOINT_TEMP = 'http://backend-app-captaintcs.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/';
-const BACKEND_ENDPOINT = process.env.BACKEND_ENDPOINT || BACKEND_ENDPOINT_TEMP;
+const BACKEND_ENDPOINT = process.env.REACT_APP_BACKEND_ENDPOINT || BACKEND_ENDPOINT_TEMP;
 
 export const BLOG_FETCH_URL = BACKEND_ENDPOINT + 'blog/getBlogDetails';
 export const BLOG_ADD_URL =  BACKEND_ENDPOINT + 'blog/saveBlog';
